3c25df89f7d973ae0ddd41664b6733e523a517ec
Anurag <anuragk3905@gmail.com>
2026-05-18 11:39:59 +0530

    frontend deployed

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,6 @@ const app = express();
 // add cors
 app.use(cors({
   origin: ['http://localhost:5173', 'https://user-management-app-tau-henna.vercel.app'],
-  methods: ['GET','POST','PATCH','DELETE','PUT','OPTIONS'],
-  credentials: true,
 }))
 
 // Add body parser middleware
